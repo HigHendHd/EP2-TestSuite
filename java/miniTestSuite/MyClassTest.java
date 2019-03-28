06db274f41483c0b2bb50a489321b2e539c9cb4d
Dennis Eigner <deahtkid@deigner.eu>
2019-03-29 00:03:41 +0100

    Made a few qol changes

--- java/miniTestSuite/MyClassTest.java
+++
@@ -1,8 +0,0 @@
-package test.java.miniTestSuite;
-
-public class MyClassTest {
-
-    protected void print(String testName) {
-        System.out.println("Testing " + getClass().getSimpleName() + " " + testName);
-    }
-}
